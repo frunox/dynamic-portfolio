b4fdc153df581435764b897c9705f2e1bfc092bb
frunox <60527588+frunox@users.noreply.github.com>
2021-02-10 08:18:04 -0600

    update About page

--- client/src/components/AboutMeComp/AboutMeComp.js
+++ client/src/components/AboutMeComp/AboutMeComp.js
@@ -9,12 +9,10 @@ function AboutMeComp() {
     <div className="aboutContainer aboutWrapper">
       <div className="container">
         <p className="top"></p>
-        <h3 className="title">About jtsy Portfolio</h3>
+        <h3 className="title">About Dynamic Portfolio</h3>
 
         <p>
-          My portfolio is built using jtsy Portfolio, an app that automatically creates a curated
-          portfolio from a developer's GitHub projects. The portfolio is presented
-          in a standardized format for potential employers, or others, to view.  Originally conceived by Shawn Hayes and co-developed with Tom van Deusen and Yeng Vang, I've taken the template and customized it to show my projects with my styling. See more about the project in the <a href="https://github.com/frunox/jtsy" rel="noopener noreferrer" target="_blank">GitHub</a> readme file.
+          My portfolio is built using <a href="https://github.com/frunox/dynamic-portfolio" rel="noopener noreferrer" target="_blank">Dynamic Portfolio</a> Dynamic Portfolio, an app that automatically creates a curated portfolio from a developer's GitHub projects. The portfolio is presented in a standardized format for potential employers, or others, to view.  Originally conceived as <a href="https://github.com/frunox/jtsy" rel="noopener noreferrer" target="_blank">jtsy Portfolio</a> by Shawn Hayes and co-developed with Tom van Deusen and Yeng Vang, I've taken the template, made it more performant, and customized it to show my projects with my styling. See more about the project in the  readme file.
       </p>
 
 
